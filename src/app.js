2768f19a165a4502c6c8a01421d6fb01145f4b00
yurimarcon <yuri.erik.oliveira@gmail.com>
2021-10-31 13:12:30 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-// import fs from 'fs';
-// import https from 'https';
+import fs from 'fs';
+import https from 'https';
 import cors from 'cors';
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(router);
 
 app.listen(process.env.PORT || 3000, ()=>console.log("Api Rodando."))
 
-// https.createServer({
-//     cert: fs.readFileSync('src/SSL/code.crt'),
-//     key: fs.readFileSync('src/SSL/code.key')
-// }, app).listen(3001, ()=> console.log("Rodando em https"));
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, ()=> console.log("Rodando em https"));
